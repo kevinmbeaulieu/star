dd9b6c63a6e95962d9501248691889a83e82b59c
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2022-04-25 14:37:20 -0400

    Fix issue with package install scripts

--- .github/scripts/install_swiftlint.sh
+++ .github/scripts/install_swiftlint.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Installs the SwiftLint package.
-# Tries to install from Homebrew, but if that fails just recompiles from source.
+# Installs the SwiftLint package if it's not already installed.
 
 set -e
 
+# Check if already installed.
+if type swiftlint > /dev/null 2>&1; then
+    exit 0
+fi
+
+# Attempted to install from Homebrew.
 brew update && brew install swiftlint
 
-if ! type swiftlint > /dev/null 2>&1; then
-    echo "Failed to install SwiftLint from Homebrew. Compiling from source..." &&
-    swiftlint_repo_path=$(mktemp swiftlint.XXXX) &&
-    git clone https://github.com/realm/SwiftLint.git $swiftlint_repo_path &&
-    cd $swiftlint_repo_path &&
-    git submodule update --init --recursive &&
-    make install
+if type swiftlint > /dev/null 2>&1; then
+    exit 0
 fi
+
+# If Homebrew installation failed, compile from source and install.
+echo "Failed to install SwiftLint from Homebrew. Compiling from source..." &&
+swiftlint_repo_path=$(mktemp swiftlint.XXXX) &&
+git clone https://github.com/realm/SwiftLint.git $swiftlint_repo_path &&
+cd $swiftlint_repo_path &&
+git submodule update --init --recursive &&
+make install
