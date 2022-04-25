e6a1aea499ca58e7e05b0a2ab4d39f7a933238e4
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2022-04-25 14:22:00 -0400

    Install swiftlint/swiftformat if needed

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -2,5 +2,8 @@
 
 set -e -u -o pipefail
 
-swiftlint lint --quiet
+sh .github/scripts/install_swiftlint.sh
+sh .github/scripts/install_swiftformat.sh
+
+swiftlint lint --quiet --strict .
 swiftformat --lint .
